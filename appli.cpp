68ce6dfbfcf431ec115015bd2cdf70d3b8040289
chipyards <noullet@chipyards.com>
2018-12-21 08:44:26 +0100

    3 tests CLI: parseur, spawner, la reunion = gdb en mode MI

--- appli.cpp
+++ appli.cpp
@@ -12,8 +12,54 @@ using namespace std;
 void usage()
 {
 printf("usage : appli d <cmd> : test spawn, process generique avec 2 pipes\n"
-       "        appli m <fil> : test parseur MI avec un fichier texte\n" );
+       "        appli m <fil> : test parseur MI avec un fichier texte\n"
+       "        appli g <target> : test spawn gdb MI avec parseur\n" );
 }
+
+// test 2 pipes avec gdb en mode mi
+int test_mi( const char * cmd )
+{
+daddy ledad;
+#define dad (&ledad)
+mi_parse lemipa;
+#define mipa (&lemipa)
+int retval;
+char tbuf[1024];
+int d;
+
+snprintf( tbuf, sizeof(tbuf), "gdb --interpreter=mi %s", cmd );
+
+retval = dad->start_child( tbuf );
+if	( retval )
+	printf("error daddy %d\n", retval );
+
+while	( fgets( tbuf, sizeof(tbuf), stdin ) )
+	{
+	if	( tbuf[0] == 'Q' )
+		break;
+	if	( tbuf[0] > ' ' )
+		dad->send_cmd(tbuf);
+	while	( ( d = dad->child_getc() ) >= 0 )
+		{
+		retval = mipa->proc1char( d );
+		if	( retval == 0 )
+			continue;
+		else if	( retval < 0 )
+			{
+			printf("err %d\n", -retval );
+			return 1;
+			}
+		else	{
+			if	( mipa->dump( retval, tbuf, sizeof(tbuf) ) )
+				printf("%s\n", tbuf );
+			}
+		}
+	fflush(stdout);
+	}
+return 0;
+}
+
+
 
 // test 2 pipes avec un child process quelconque
 int test_daddy( const char * cmd )
@@ -104,6 +150,7 @@ switch	( argv[1][0] )
 	{
 	case 'd' : test_daddy( argv[2] ); break;
 	case 'm' : test_parseur( argv[2] ); break;
+	case 'g' : test_mi( argv[2] ); break;
 	default: usage();
 	}
 return 0;
